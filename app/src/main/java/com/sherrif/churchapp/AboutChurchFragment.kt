5e95f8a01fa502cd73d5cb243515c2d437df1f84
kimitasharrif <kimitasharrif@gmail.com>
2024-09-20 14:58:11 +0300

    second commit

--- app/src/main/java/com/sherrif/churchapp/AboutChurchFragment.kt
+++ app/src/main/java/com/sherrif/churchapp/AboutChurchFragment.kt
@@ -5,55 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.textview.MaterialTextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AboutChurchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AboutChurchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_church, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_about_church, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AboutChurchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AboutChurchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // Example texts for ministry and core messages
+        val originalMinistryMessage = "To bring people to Christ and to the membership of His family, disciple and equip them (Ephesians 4:12)\r\n\r\nTo add value in people's lives, through teaching and biblical principles and being a role model,\r\n\r\nTo develop a Kingdom community that impacts other communities.\r\nExample Academic Community\r\n and Business Community"
+        val originalCoreMessage = "We are a family.\r\nExcellence in Service.\r\nSound Biblical Teachings.\r\nLearning.\r\nGod Centered worship.\r\nChildren Ministry.\r\n"
+
+        // Get the TextViews from the inflated layout
+        val ministryMessageTextView = view.findViewById<MaterialTextView>(R.id.ministry_message)
+        val coreMessageTextView = view.findViewById<MaterialTextView>(R.id.core_message)
+
+        // Remove the unwanted characters and set the text
+        val ministryMessage = originalMinistryMessage.replace("\r\n", "\n")
+        ministryMessageTextView.text = ministryMessage
+
+        val coreMessage = originalCoreMessage.replace("\r\n", "\n")
+        coreMessageTextView.text = coreMessage
+
+        return view
     }
 }
--- app/src/main/java/com/sherrif/churchapp/MainActivity.kt
+++ app/src/main/java/com/sherrif/churchapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.sherrif.churchapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,6 +14,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.sherrif.churchapp.databinding.ActivityMainBinding
+import com.sherrif.churchapp.helpers.PrefsHelper
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,8 +37,9 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+
         // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // menu should be considered as top-level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_order, R.id.nav_about, R.id.nav_profile
@@ -49,6 +53,35 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                openSettings()
+                true
+            }
+            R.id.logout -> {
+                logoutUser()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun openSettings() {
+        // Code to open settings screen
+    }
+
+    private fun logoutUser() {
+        // Clear shared preferences
+        PrefsHelper.clearPrefs(this)
+
+        // Start MainActivity (or login screen)
+        startActivity(Intent(this, MainActivity::class.java))
+
+        // Finish all activities
+        finishAffinity()
     }
 
     override fun onSupportNavigateUp(): Boolean {
